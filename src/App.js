48d00f6afc52bb97f5360c650aa279000e8b40c9
Rendy Artha <rendyartha312@gmail.com>
2021-03-10 14:04:51 +0700

    release-1 done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
-import './App.css';
+import React, {useState} from "react";
+import { Container } from "react-bootstrap";
+import FormOrder from "./components/FormOrder";
+import OrderPage from "./components/OrderPage";
 
 function App() {
-  return (
-    <div className="App">
+  const [values, setValues] = useState(
+    {
+      namaBarang: "",
+      jumlahBarang: "",
+      hargaBarang: ""
+    }
+  )
 
+  const [listInputItem, setListInputItem] = useState([])
+
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setValues({
+      ...values,
+      [name]: value
+    })
+  }
+
+  const handleAddItem = (e) => {
+    e.preventDefault();
+    if(values.namaBarang === "" || values.jumlahBarang === "" || values.hargaBarang === "") {
+      alert("Please input your data")
+    } else {
+      console.log(values)
+      setValues({
+        namaBarang: "",
+        jumlahBarang: "",
+        hargaBarang: ""
+      })
+    }
+  }
+
+  return (
+    <div className="d-flex ">
+      <Container>
+        <h1 className="align-items-center text-center">Kasir - Apps</h1>
+        <FormOrder 
+          values = {values} 
+          handleChange = {handleChange}
+          handleAddItem = {handleAddItem}
+        />
+        <OrderPage />
+      </Container>
     </div>
   );
 }
---
+++ src/components/FormOrder.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Container, Form, Button } from 'react-bootstrap'
+
+const FormOrder = ({values, handleChange, handleAddItem}) => {
+  return (
+    <Container>
+      <Form onSubmit={handleAddItem}>
+        <Form.Group>
+          <Form.Label>Nama barang</Form.Label>
+          <Form.Control 
+            type="text" 
+            placeholder="Masukkan nama barang..."
+            name="namaBarang"
+            value={values.namaBarang}
+            onChange={handleChange}
+          />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Jumlah barang</Form.Label>
+          <Form.Control 
+            type="number" 
+            placeholder="Masukkan jumlah barang anda"
+            name="jumlahBarang"
+            value={values.jumlahBarang}
+            onChange={handleChange}
+          />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Harga barang</Form.Label>
+          <Form.Control 
+            type="number"
+            placeholder="Masukkan harga barang anda"
+            name="hargaBarang"
+            value={values.hargaBarang}
+            onChange={handleChange}
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  )
+}
+
+export default FormOrder
